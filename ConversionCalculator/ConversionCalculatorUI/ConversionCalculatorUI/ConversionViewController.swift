d6bf41157b2f1a7fa2f3d9bdc62370cdfff1e74a
Jackson Chandler <jackson.c.chandler@gmail.com>
2017-12-11 20:32:50 -0600

    init

---
+++ ConversionCalculator/ConversionCalculatorUI/ConversionCalculatorUI/ConversionViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConversionCalculatorUI
+//
+//  Created by Chandler, Jackson C. (MU-Student) on 12/11/17.
+//  Copyright © 2017 Chandler, Jackson C. (MU-Student). All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
